388427b79315ae7e1b5c229d684ebbbf7de6756a
WHITE_TIGER_BOTZ_INC <imsehpasidu@gmail.com>
2024-10-27 17:50:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lyFkVJ7C#AcdQAzg20_DVbMbLs-sigfgN10n-zuRZZAP5bLQ39Uw",
+SESSION_ID: process.env.SESSION_ID || "DM02BDjR#LiMhRAJADHyp1Q7Y6OAEYdNZd6uyUyFLn8afou3sQYw",
 MONGODB: process.env.MONGODB || "mongodb+srv://user:321465@cluster0.7jcofhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //enter mongo db url
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/iuwjg1.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now"
